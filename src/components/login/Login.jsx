135f4a6fa4d315815628345a5c1f78199884f234
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-22 00:01:41 +0600

    Authentication with google setup complete

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,19 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+    const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         console.log("Google sign in clicked");
+        signInWithPopup(auth, provider)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
     return (
         <div>
