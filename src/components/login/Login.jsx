2ba5e4707228bf2a713d5ff892ee05f6e74328f0
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-21 23:44:00 +0600

    Sign In button Added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleGoogleSignIn = () => {
+        console.log("Google sign in clicked");
+    }
     return (
         <div>
             <h3>Please Log in first</h3>
+            <button onClick={handleGoogleSignIn}>Sing In With Google</button>
         </div>
     );
 };
