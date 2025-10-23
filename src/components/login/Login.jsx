58d8feb0b23219164de8acbb046d71169f5cb0fa
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-23 23:27:52 +0600

    User Information Displayed

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,17 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
     const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null);
 
     const handleGoogleSignIn = () => {
         console.log("Google sign in clicked");
         signInWithPopup(auth, provider)
         .then(result => {
-            console.log(result);
+            console.log(result.user);
+            setUser(result.user);
         })
         .catch(error => {
             console.log(error);
@@ -19,6 +21,13 @@ const Login = () => {
         <div>
             <h3>Please Log in first</h3>
             <button onClick={handleGoogleSignIn}>Sing In With Google</button>
+            {
+                user && <div>
+                    <p>Name: {user.displayName}</p>
+                    <p>Email: {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
